be71d9f9e60577d6dd84788c7b06c1ddea154b2e
Matias Melgarejo <matizeta15@gmail.com>
2022-04-18 18:08:52 -0300

    ciclos

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+let palabra = prompt("escriba una palabra");
+let repeticiones = prompt("Ingrese cuantas veces quiere que se repita la palabra");
+
+for(let i=1; i<=repeticiones; i++){
+
+    console.log(i+"- "+ palabra);
+    
+}
